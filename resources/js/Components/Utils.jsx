11c9cee4f7bbea06161252bbd5bc5991ae7d0d63
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-06 14:13:10 +0700

    Getting all permissions from props auth

--- resources/js/Components/Utils.jsx
+++ resources/js/Components/Utils.jsx
@@ -1,9 +1,12 @@
 import { usePage } from "@inertiajs/react";
 
-export default function hasAnyPermission(){
+export default function hasAnyPermission(permissions){
 
     // destruct auth from usepage props
     const { auth } = usePage().props
+    
+    // get all permissions from props auth
+    let allPermissions = auth.permissions;
 
 
  return;
