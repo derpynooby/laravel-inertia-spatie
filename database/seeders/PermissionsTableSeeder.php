7691345fccb0c12e2861c9410af08b88a6db769b
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-06 07:48:00 +0700

    Adding permissions for users in permission table seeder

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class PermissionsTableSeeder extends Seeder
@@ -12,6 +13,11 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // permission for users
+        Permission::create(['name' => 'users index', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users create', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users edit', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users delete', 'guard_name' => 'web']);
+
     }
 }
