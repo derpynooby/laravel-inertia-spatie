bbca62f75d5c4d2d2cffbf9ff898673994ed1cc2
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-06 07:49:34 +0700

    Adding permission for roles in permission table seeder

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -19,5 +19,11 @@ class PermissionsTableSeeder extends Seeder
         Permission::create(['name' => 'users edit', 'guard_name' => 'web']);
         Permission::create(['name' => 'users delete', 'guard_name' => 'web']);
 
+        // permission for roles
+        Permission::create(['name' => 'roles index', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles create', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles edit', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles delete', 'guard_name' => 'web']);
+
     }
 }
