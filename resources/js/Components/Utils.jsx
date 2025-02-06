89f64e14a426eeb3ae33081db0ff36e054a13543
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-06 14:11:36 +0700

    Destruct auth from usepage props

---
+++ resources/js/Components/Utils.jsx
@@ -0,0 +1,12 @@
+import { usePage } from "@inertiajs/react";
+
+export default function hasAnyPermission(){
+
+    // destruct auth from usepage props
+    const { auth } = usePage().props
+
+
+ return;
+
+   
+}
