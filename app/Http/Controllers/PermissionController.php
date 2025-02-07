23befbead829ea0262689e30d9ef1ee50ea4b436
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-07 10:26:24 +0700

    Destroy/deleting permission data from database (function destroy)

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -87,8 +87,12 @@ class PermissionController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Permission $permission)
     {
-        //
+        // delete permissions data
+        $permission->delete();
+
+        // render view
+        return back();
     }
 }
