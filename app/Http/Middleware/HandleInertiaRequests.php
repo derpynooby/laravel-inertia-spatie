2c7d83b3be9953541352c3b8ea09e9b53b620903
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-08 15:05:31 +0700

    Adding permission to auth

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
-use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -32,12 +31,10 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            // getting user permission and adding it to authentication
             'auth' => [
                 'user' => $request->user(),
-            ],
-            'ziggy' => fn () => [
-                ...(new Ziggy)->toArray(),
-                'location' => $request->url(),
+                'permissions' => $request->user() ? $request->user()->getUserPermissions() : [],
             ],
         ];
     }
