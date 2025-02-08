3ee4945b2b1d78458652228d1f6bbd3cd44f4bd0
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-08 08:39:21 +0700

    Destroy/delete role data from database (function destroy)

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -136,8 +136,12 @@ class RoleController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        //
+        // delete role data
+        $role->delete();
+
+        // render view
+        return back();
     }
 }
