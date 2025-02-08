423c69bda22eac0f26bc5a17de133a4e9d6fd2bc
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-08 09:14:48 +0700

    Assingning or adding middleware for every routes / permissions to user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
 class UserController extends Controller
 {
+    // Assingning or adding middleware for every routes / permissions
+    public static function middleware()
+    {
+        return [
+            new Middleware('permission:users index', only : ['index']),
+            new Middleware('permission:users create', only : ['create', 'store']),
+            new Middleware('permission:users edit', only : ['edit', 'update   ']),
+            new Middleware('permission:users delete', only : ['destroy']),
+        ];
+    }
     /**
      * Display a listing of the resource.
      */
