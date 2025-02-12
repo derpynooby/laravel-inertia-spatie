0e7d169c46f5136a13a1e44514ff5189632b10be
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-12 12:06:29 +0700

    Editing post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'post',
+    ];
+
+    /**
+     * The attributes that should be guarded for serialization.
+     *
+     * @var list<string>
+     */
+    protected $guarded = [
+        'id',
+    ];
 }
